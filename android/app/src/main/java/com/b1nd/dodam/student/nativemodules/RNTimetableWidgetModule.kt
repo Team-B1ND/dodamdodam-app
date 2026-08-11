f278d289537d5d2d9827e227a6fcc145763532b5
parkhj2009 <eee0110010100@gmail.com>
2026-08-11 14:18:04 +0900

    feat(widget): 시간표 데이터 저장 및 위젯 갱신 연동

--- android/app/src/main/java/com/b1nd/dodam/student/nativemodules/RNTimetableWidgetModule.kt
+++ android/app/src/main/java/com/b1nd/dodam/student/nativemodules/RNTimetableWidgetModule.kt
@@ -1,5 +1,6 @@
 package com.b1nd.dodam.student.nativemodules
 
+import com.b1nd.dodam.student.widget.TimetableWidgetProvider
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -9,6 +10,12 @@ class RNTimetableWidgetModule(reactContext: ReactApplicationContext) : ReactCont
 
   @ReactMethod
   fun saveTimetable(json: String) {
+    reactApplicationContext
+      .getSharedPreferences(TimetableWidgetProvider.PREFERENCES_NAME, 0)
+      .edit()
+      .putString(TimetableWidgetProvider.TIMETABLE_KEY, json)
+      .apply()
+    TimetableWidgetProvider.updateAll(reactApplicationContext)
     WatchDataSender.send(reactApplicationContext, "/dodam/timetable", json)
   }
 }
